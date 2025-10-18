7bec4092f0826eb3b0afd21bd8ad3248619f56ef
Fabian <fabidiaz2804@gmail.com>
2025-10-17 21:08:43 -0300

    finalizacion metodos (add,get y remove) de SparseMatrix

--- SparseMatrix.cpp
+++ SparseMatrix.cpp
@@ -14,15 +14,61 @@ void SparseMatrix::add(int value, int xPos, int yPos) {
     Nodo* actual = start;
     Nodo* anterior= nullptr;
 
+    while (actual!=nullptr(actual -> x <xPos || actual -> x == xPos && actual -> y <yPos)) {
+        anterior = actual;
+        actual = actual -> next;
+
+    }
+    //por si existe ya un nodo en esa posicion borramos el nodo nuevo pq ya esta ocupada solo actualizamos su valor
+    if (actual -> x == xPos && actual -> y == yPos) {
+        actual -> value = value;
+        delete nuevoNodo;
+        return;
+    }
+    //para ponerlo entre el anterior y el actual (anterior vacio actual) en vacio seria
+    if (anterior == nullptr) { // si el anterior esta en nulo lo ponemos en el principio entonces seria nuevo , start (nuevo | start )
+        nuevoNodo -> next = start;
+        start = nuevoNodo;
+    }else { //si tiene anterior hacemos lo de ponerlo entremedio de los 2
+        anterior -> next = nuevoNodo;
+        nuevoNodo -> next = actual;
+
+    }
+
 }
 
 int SparseMatrix::get(int x, int y) {
+    Nodo* actual = start;
+    while (actual!=nullptr) {
+        if (actual -> x == x && actual -> y == y) {
+            return actual -> value;
+        }
+        if (actual ->x >x || actual -> x == x && actual -> y >y) {
+            actual = actual -> next;
 
+        }
+    }
+    return 0;
 }
 
 
 void SparseMatrix::remove(int xPos, int yPos) {
+ Nodo* actual = start;
+    Nodo* anterior = nullptr;
+    while (actual!=nullptr) {
+        if (actual -> x == xPos && actual -> y == yPos) {
+            if (anterior!=nullptr) {
+                anterior -> next = actual -> next;
+            }else {
+                start = actual -> next;
 
+            }
+            delete actual;
+            return;
+        }
+        anterior = actual;
+        actual = actual -> next;
+    }
 }
 
 
