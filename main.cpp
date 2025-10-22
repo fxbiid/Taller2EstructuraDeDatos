fbff36d12713bc8c49cdced8e782abc6d94ec9b3
Fabian <fabidiaz2804@gmail.com>
2025-10-21 23:55:48 -0300

    Finalizacion de main para pruebas

--- main.cpp
+++ main.cpp
@@ -26,9 +26,41 @@ int main() {
         cout<<"add"<<n<<" = "<<tiempoFin<<" seg"<<endl;
     }
     //prueba en get
-
+    cout<<"\n";
+    cout<<"Pruebas en funcion get"<<endl;
+    for (int i = 0; i < 5; i++) {
+        int n = cants[i];
+        SparseMatrix matrix2;
+        for (int x = 0; x < n; x++) {
+            matrix2.add(rand()+1 % 100, rand() % 100, rand() % 100);
+        }
+        clock_t inicioPruebaGet = clock();
+        for (int j = 0; j < n; j++) {
+            matrix2.get(rand() % 100, rand() % 100);
+        }
+        clock_t finPruebaGet = clock();
+        double tiempoFinGet = double(finPruebaGet-inicioPruebaGet)/CLOCKS_PER_SEC;
+        cout<<"get"<<n<<" = "<<tiempoFinGet<<" seg"<<endl;
+    }
     //prueba en multiply
+    cout<<"\n";
+    cout<<"Pruebas en funcion multiply"<<endl;
+    for (int i = 0; i < 5; i++) {
+        int n = cants[i];
+        SparseMatrix A,B;
+        for (int x = 0; x < n; x++) {
+            A.add(rand() % 100, rand() % 100, rand() % 100);
+            B.add(rand() % 100, rand() % 100, rand() % 100);
+        }
+        clock_t inicioPruebaMulti = clock();
+        SparseMatrix* C = A.multiply(&B);
+        clock_t finPruebaMulti = clock();
+        double tiempoFinMulti = double(finPruebaMulti-inicioPruebaMulti)/CLOCKS_PER_SEC;
+        cout<<"multiply"<<n<<" = "<<tiempoFinMulti<<" seg"<<endl;
 
+        delete C;
+
+    }
 
     return 0;
 }
